9e692b3fd604b242e101b75e0f6e2c014bf54ead
Punisher385 <vasykhnikdenus@gmail.com>
2024-11-25 01:15:13 +0200

    Add files via upload

---
+++ Васюхник1.py
@@ -0,0 +1,8 @@
+greeting = "Hello world"
+name = "Денис"
+surname = "Васюхник"
+age = 16
+print("greeting:", greeting, "- Тип:", type(greeting))
+print("name:", name, "- Тип:", type(name))
+print("surname:", surname, "- Тип:", type(surname))
+print("age:", age, "- Тип:", type(age))
