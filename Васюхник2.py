a6ea9add58f5a8aa3a0139dd5fec857792f4a237
Punisher385 <vasykhnikdenus@gmail.com>
2024-11-25 01:17:06 +0200

    Add files via upload

---
+++ Васюхник2.py
@@ -0,0 +1,7 @@
+name = "Денис"
+surname = "Васюхник"
+age = 16
+if type(name) == type(surname) == type(age):
+    print(f"{name} {surname}")
+    print(f"{type(name)} {type(surname)}")
+    print(f"{type(name)} {type(age)}")
